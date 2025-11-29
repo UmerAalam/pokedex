9c269d27a472169f6fb6620a9da3d2f103c0c90c
Umer Aalam <umerrazzaq.dev@gmail.com>
2025-11-29 13:35:53 +0500

    maping-pokemon-results

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 
 interface Pokemon {
   name: string;
+  url: string;
 }
-
+interface PokemonList {
+  count: number;
+  next: string;
+  previous: string;
+  results: Pokemon[];
+}
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>();
 
   useEffect(() => {
     fetchPokemons();
   }, []);
+
   const fetchPokemons = async () => {
     try {
       const response = await fetch(
         "https://pokeapi.co/api/v2/pokemon/?limit=20",
       );
-      const data: Pokemon[] = await response.json();
-      setPokemons(data);
+      const data: PokemonList = await response.json();
+      setPokemons(data.results);
     } catch (e) {
       console.log(e);
     }
   };
-  return <ScrollView></ScrollView>;
+  return (
+    <ScrollView>
+      {pokemons?.map((pokemon) => {
+        return (
+          <View key={pokemon.name}>
+            <Text>{pokemon.name}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
 }
